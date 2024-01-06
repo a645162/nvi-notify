634507266921b245f871fad26bde9a7ab5b5475a
Haomin Kong <a645162@gmail.com>
2024-01-06 23:07:56 +0800

    update env var

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from monitor import nvitop_monitor
 from web import flask_main
+from utils import env
+
+import time
 
 if __name__ == '__main__':
+    print("GPU MONITOR")
+    print("=" * 40)
+    print("GPU_MONITOR_WEBHOOK_WEWORK")
+    print(env.get_env("GPU_MONITOR_WEBHOOK_WEWORK"))
+    print("GPU_MONITOR_SLEEP_TIME_START")
+    print(env.get_env_time("GPU_MONITOR_SLEEP_TIME_START"))
+    print("GPU_MONITOR_SLEEP_TIME_END")
+    print(env.get_env_time("GPU_MONITOR_SLEEP_TIME_END"))
+    print("=" * 40)
+    print("Program will start in 60 seconds...")
+    time.sleep(60)
+    print("=" * 40)
+    print("Program starting...")
+
     nvitop_monitor.start_monitor_all()
     flask_main.start_web_server()

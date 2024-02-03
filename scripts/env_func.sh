46869067741a777a45ffcb3eb83de1059d95dd6b
Haomin Kong <a645162@qq.com>
2024-02-03 18:55:49 +0800

    update Start Script

--- scripts/env_func.sh
+++ scripts/env_func.sh
@@ -1,11 +1,12 @@
 # Function to check and load environment variable with a default value
-function check_and_load_env_variable {
+function CheckAndLoadEnvVariable {
     local env_variable_name="$1"
     local default_value="$2"
 
     # Check if the environment variable exists
     if [[ -z "${!env_variable_name}" ]]; then
-        echo "Environment variable '$env_variable_name' not found. Loading it with default value: '$default_value'"
+        echo "Environment variable '$env_variable_name' not found."
+        echo "    Loading it with default value: '$default_value'"
 
         # Load the environment variable with the default value
         export "$env_variable_name"="$default_value"

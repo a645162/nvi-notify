3dac29a251f1fa29792946eb092a95ba84b73408
Haomin Kong <a645162@qq.com>
2024-06-04 11:22:02 +0800

    Fix bug

--- notify/group_center.py
+++ notify/group_center.py
@@ -5,11 +5,11 @@ logger = get_logger()
 
 
 def gpu_monitor_start():
-    pass
+    logger.info("[Group Center] Gpu Monitor Start")
 
 
 def gpu_task_message(process_obj, task_status: str):
     from monitor.GPU.gpu_process import GPUProcessInfo
     process_obj: GPUProcessInfo = process_obj
 
-    print("gpu_task_message pid:", process_obj.pid)
+    logger.info(f"gpu_task_message pid:{process_obj.pid}")

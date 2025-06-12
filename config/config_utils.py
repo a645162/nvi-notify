6f55a5bcfcde96a908292b0df6ac08c79480ac0c
YconfigY <yang981ha@foxmail.com>
2025-06-12 20:33:32 +0800

    fix(config_utils): 更新 is_webhook_sleep_time 函数参数
    - 为 is_webhook_sleep_time 函数的 start_time 和 end_time 参数添加默认值 None

--- config/config_utils.py
+++ config/config_utils.py
@@ -8,7 +8,9 @@ from feature.utils.common_utils import do_command
 logger = get_logger()
 
 
-def is_webhook_sleep_time(start_time: datetime.time, end_time: datetime.time) -> bool:
+def is_webhook_sleep_time(
+    start_time: datetime.time = None, end_time: datetime.time = None
+) -> bool:
     if start_time is None or end_time is None:
         from config.settings import WEBHOOK_SLEEP_TIME_END, WEBHOOK_SLEEP_TIME_START
 

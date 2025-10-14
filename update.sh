6ae8bbccc0993fed4840f0c9ebe2f62f5992cac9
Haomin Kong <a645162@qq.com>
2025-10-14 11:34:16 +0800

    fix(update): 更新依赖版本并确保激活conda环境

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+conda activate nvitop
+
 git pull
 
-pip install -U li-group-center
+pip install -U "li-group-center>=2.5.1" -i https://pypi.python.org/simple
 
 pip install -r requirements.txt
